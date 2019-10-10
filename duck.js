7baddf77e9d78e513bcb1e8a2f5bd66217247775
coudert <jean.baptiste.coudert1@gmail.com>
2019-10-10 16:55:24 +0200

    finish function

--- duck.js
+++ duck.js
@@ -15,7 +15,20 @@ var btn = document.createElement("button");
 document.body.appendChild(btn);
 btn.innerHTML = "Click for Play";
 
+function setRandomPosition(duck) {
+    var posX = Math.floor(Math.random() * 550);
+    var posY = Math.floor(Math.random() * 550);
+    duck.style.left = posX + "px";
+    duck.style.top = posY + "px";
+}
+function createDuck() {
+    var duckElement = document.createElement("div");
+    zoneGame.appendChild(duckElement);
+    duckElement.setAttribute("class", "duck");
+    duckElement.style.backgroundColor = getRandomColor();
 
+    return duckElement
+}
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -27,39 +40,26 @@ function getRandomColor() {
 
 
 function frame() {
-    var allDucks = document.getElementsByClassName("ducks")
+    var allDucks = document.getElementsByClassName("duck")
     for (let i = 0; i < allDucks.length; i++) {
-        var posX = Math.floor(Math.random() * 550);
-        var posY = Math.floor(Math.random() * 550);
-
-        allDucks[i].style.left = posX + "px";
-        allDucks[i].style.top = posY + "px";
+        setRandomPosition(allDucks[i])
         allDucks[i].style.transitionDuration = Math.floor(Math.random() * 4 + 1) + "s"
     }
 }
 
 function killduck() {
     this.remove()
-    var duck = document.createElement("div");
-    zoneGame.appendChild(duck);
-    duck.setAttribute("class", "ducks");
-    duck.style.backgroundColor = getRandomColor();
+    var duck = createDuck()
     duck.addEventListener("click", killduck);
-    var posX = Math.floor(Math.random() * 550);
-    var posY = Math.floor(Math.random() * 550);
-    duck.style.left = posX + "px";
-    duck.style.top = posY + "px";
+    setRandomPosition(duck)
     compteur.innerHTML = cpt++
 }
 
 btn.addEventListener("click", function () {
     for (let i = 0; i < 10; i++) {
-        var duck = document.createElement("div");
-        zoneGame.appendChild(duck);
-        duck.setAttribute("class", "ducks");
-
+        var duck = createDuck()
+        setRandomPosition(duck)
         duck.style.backgroundColor = getRandomColor();
-
         duck.addEventListener("click", killduck);
     }
     setInterval(frame, 2000)
